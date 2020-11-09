5fdfaa69710673d6c2df98232b5adb8ef65f8ecf
soshimee <45235204+soshimee@users.noreply.github.com>
2020-11-10 01:27:51 +0900

    Create logout.js

---
+++ logout.js
@@ -0,0 +1,3 @@
+document.querySelector("[aria-label='User Settings']").click();
+Array.from(document.querySelectorAll('div')).find(el => el.textContent === 'Log Out').click();
+Array.from(document.querySelectorAll('div.contents-18-Yxp')).find(el => el.textContent === 'Log Out').click();
